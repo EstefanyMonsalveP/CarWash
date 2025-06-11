61acd80d7d03d286b4ec5063b31e7eec57a965d5
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-06-10 20:06:35 -0500

    feat(FacturasConServiciosDTO.cs): Clase para recibir listas desde la interfaz

---
+++ Backend/Models/DTOs/FacturasConServiciosDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_lavadero.Models.DTOs
+{
+    public class FacturasConServiciosDTO
+    {
+        public FACTURA Factura { get; set; }
+        public List<FACTURA_SERVICIO> Servicios { get; set;}
+    }
+}
