058d0837e0e8dda5a138180c0b43487e4c922334
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-06-12 01:02:31 -0500

    feat(FormasPagoController.cs): Creación del controlador para la clase de clsFormaPago

---
+++ Backend/Controllers/FormasPagoController.cs
@@ -0,0 +1,21 @@
+﻿using Servicios_lavadero.Clases;
+using Servicios_lavadero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios_lavadero.Controllers
+{
+    [EnableCors(origins: "https://localhost:44315", headers: "*", methods: "*")]
+    public class FormasPagoController: ApiController
+    {
+        public List<FORMA_PAGO> get()
+        {
+            clsFormaPago _clsFormaPago = new clsFormaPago();
+            return _clsFormaPago.ConsultarMetodosPago();
+        }
+    }
+}
