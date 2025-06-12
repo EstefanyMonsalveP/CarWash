d5b2583fe99e5a69068378ae25c2ee13beb9dcd3
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-06-12 00:57:02 -0500

    feat(clsFormaPago.cs): Clase para el modelo de forma de pago

---
+++ Backend/Clases/clsFormaPago.cs
@@ -0,0 +1,22 @@
+﻿using Servicios_lavadero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_lavadero.Clases
+{
+    public class clsFormaPago
+    {
+        private LAVADEROEntities dbLavadero = new LAVADEROEntities();
+
+        public FORMA_PAGO formaPago { get; set; }
+
+        public List<FORMA_PAGO> ConsultarMetodosPago()
+        {
+            return dbLavadero.FORMA_PAGO
+                .OrderBy(fp => fp.DESCRIPCION_PAGO)
+                .ToList();
+        }
+    }
+}
