3de082c9bf7b4d2f370e2d33c88ff84ff45758aa
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-06-11 21:55:22 -0500

    feat(clsServicio.cs): Clase para listar los servicios de la BD

---
+++ Backend/Clases/clsServicio.cs
@@ -0,0 +1,21 @@
+﻿using Servicios_lavadero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_lavadero.Clases
+{
+    public class clsServicio
+    {
+        private LAVADEROEntities dbLavadero = new LAVADEROEntities();
+        public SERVICIO _servicio { get; set; }
+
+        public List<SERVICIO> ConsultaServicios()
+        {
+            return dbLavadero.SERVICIOS
+                .OrderBy(s=> s.DESCRIPCION)
+                .ToList();
+        }
+    }
+}
