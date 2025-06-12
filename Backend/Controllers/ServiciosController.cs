0aa481bf08e2ab529e63bc9f7edbff8284086a5c
EstefanyMonsalveP <monsalvepinoe@gmail.com>
2025-06-11 21:59:54 -0500

    feat(ServiciosController.cs): Controlador para manejas las solicitudes de clsServicio

---
+++ Backend/Controllers/ServiciosController.cs
@@ -0,0 +1,22 @@
+﻿using Servicios_lavadero.Clases;
+using Servicios_lavadero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios_lavadero.Controllers
+{
+    [EnableCors(origins: "https://localhost:44315", headers: "*", methods: "*")]
+    public class ServiciosController: ApiController
+    {
+        //GET
+        public List<SERVICIO> get()
+        {
+            clsServicio _clsServicio = new clsServicio();
+            return _clsServicio.ConsultaServicios();
+        }
+    }
+}
